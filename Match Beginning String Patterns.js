23cc556d18c3ff3c9fe0391e2e4c2de267082e7f
RinalisDS <denispilyutin@gmail.com>
2022-03-18 01:33:14 +0200

    +

---
+++ Match Beginning String Patterns.js
@@ -0,0 +1,11 @@
+let firstString = "Ricky is first and can be found.";
+let firstRegex = /^Ricky/;
+console.log(firstRegex.test(firstString));
+let notFirst = "You can't find Ricky now.";
+console.log(firstRegex.test(notFirst));
+
+
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // Change this line
+let result = calRegex.test(rickyAndCal);
+console.log(result);
